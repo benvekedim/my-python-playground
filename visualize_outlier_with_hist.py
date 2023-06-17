b4e1f0f4a21b8f0dd14edcc56174537bb9be5ca0
Mustafa Eroğlu <85462810+benvekedim@users.noreply.github.com>
2023-06-17 23:44:00 +0300

    Update visualize_outlier_with_hist.py

--- visualize_outlier_with_hist.py
+++ visualize_outlier_with_hist.py
@@ -4,3 +4,13 @@ import numpy as np
 data = np.array([241, 3, 46, 49, 90, 87, 24, 50, 21, 56])
 plt.hist(data, bins=10)
 plt.show()
+
+#manuel bins parametresi bulma
+
+iqr = np.percentile(data, 75) - np.percentile(data, 25)
+bin_width = 2 * iqr / (len(data) ** (1/3))
+bins = int((np.max(data) - np.min(data)) / bin_width)
+bins
+
+plt.hist(data, bins=5)
+plt.show()
