e79047830e836950b86dbe8ff3d0a5922a58086e
Mustafa Eroğlu <85462810+benvekedim@users.noreply.github.com>
2023-06-23 16:26:37 +0300

    Update df_column_clean.py

--- df_column_clean.py
+++ df_column_clean.py
@@ -2,7 +2,11 @@
 df = pd.DataFrame({'Product ID': ["a123", "b143", "c234"]})
 
 # Product ID kolonunu temizle ve yeni kolonu oluştur
-df['Product ID Cleaned'] = df['Product ID'].str.extract('(\d+)', expand=False).astype(int)
+# df['Product ID Cleaned'] = df['Product ID'].str.extract('(\d+)', expand=False).astype(int)
+
+# Eğer yeni kolon oluşmasını istemiyorsak
+# "Product ID" sütunundaki harfleri kaldırarak aynı sütunu üzerine yaz
+df['Product ID'] = df['Product ID'].str.replace('[^0-9]', '', regex=True).astype(int)
 
 # Yeni DataFrame'i yazdır
 print(df)
